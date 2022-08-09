24e61a0041ecbec1817f0fe95ad256a56bf10ce2
Mike Mortensen <mike2137@elevcampus.dk>
2022-08-09 12:09:44 +0200

    Changes

--- CSharp_10_Features/CSharp_10_Features.Application/Program.cs
+++ CSharp_10_Features/CSharp_10_Features.Application/Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+MyRecordStruct record1 = new MyRecordStruct("123", "321");
+MyStruct struct1 = new MyStruct("123", "321");
+
+Console.WriteLine(record1.ToString());
+Console.WriteLine(struct1.ToString());
+
+public record struct MyRecordStruct(string Prop1, string Prop2);
+
+public struct MyStruct : IEquatable<MyStruct>
+{
+    public MyStruct(string prop1, string prop2)
+    {
+        Prop1 = prop1;
+        Prop2 = prop2;
+    }
+
+    public string Prop1 { get; set; }
+    public string Prop2 { get; set; }
+
+    private bool PrintMembers(StringBuilder builder)
+    {
+        builder.Append(nameof(Prop1));
+        builder.Append(" = ");
+        builder.Append(Prop1);
+        builder.Append(", ");
+
+        builder.Append(nameof(Prop2));
+        builder.Append(" = ");
+        builder.Append(Prop2);
+
+        return true;
+    }
+
+    public bool Equals(MyStruct other)
+    {
+        return other is MyStruct @struct && Equals(@struct);
+    }
+
+    public override string ToString()
+    {
+        var builder = new StringBuilder();
+        builder.Append(nameof(MyStruct));
+        builder.Append(" { ");
+
+        if (PrintMembers(builder))
+            builder.Append(" ");
+
+        builder.Append("}");
+        return builder.ToString();
+    }
+
+    public override bool Equals(object obj)
+    {
+        return obj is MyStruct @struct && Equals(@struct);
+    }
+
+    public override int GetHashCode()
+    {
+        throw new NotImplementedException();
+    }
+}
